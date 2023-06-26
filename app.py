005f713e22f88dae9ea2b6dada15e3b52f118acd
AMIT KUMAR <115937880+Amit8281@users.noreply.github.com>
2023-06-26 23:26:54 +0530

    remove password

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def index():
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
 
-            client = pymongo.MongoClient("mongodb+srv://Amit8281:Amit8281@cluster0.jwsyedo.mongodb.net/?retryWrites=true&w=majority")
+            client = pymongo.MongoClient("mongodb+srv://Amit8281:pass@cluster0.jwsyedo.mongodb.net/?retryWrites=true&w=majority")
             db = client['review_scrap']
             review_col = db['review_scrap_data']
             review_col.insert_many(reviews)
